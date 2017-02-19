65cbcbc55a0a0831dc67720e48006303066718bf
Asad Quraishi <asad@cingularity.ca>
2017-02-18 22:18:23 -0500

    undistort, color, warp

--- find_lanes.py
+++ find_lanes.py
@@ -1,10 +1,53 @@
 import pickle
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('qt5agg')
-import undistort
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+def pipeline(img, s_thresh=(100, 255), sx_thresh=(12, 255), sy_thresh=(25, 255), v_thresh=(50, 255)):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS).astype(np.float)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype(np.float)
+    l_channel = hls[:, :, 1]
+    s_channel = hls[:, :, 2]
+    v_channel = hsv[:, :, 2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)  # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobelx = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
+    # Sobel y
+    sobely = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)  # Take the derivative in x
+    abs_sobely = np.absolute(sobely)  # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobely = np.uint8(255 * abs_sobely / np.max(abs_sobely))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobelx)
+    sxbinary[(scaled_sobelx >= sx_thresh[0]) & (scaled_sobelx <= sx_thresh[1])] = 1
+
+    # Threshold y gradient
+    sybinary = np.zeros_like(scaled_sobely)
+    sybinary[(scaled_sobely >= sy_thresh[0]) & (scaled_sobely <= sy_thresh[1])] = 1
+
+    # Threshold S channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
+    # Threshold V channel
+    v_binary = np.zeros_like(v_channel)
+    v_binary[(v_channel >= v_thresh[0]) & (v_channel <= v_thresh[1])] = 1
+
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    # color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, v_binary))
+    c_binary = np.zeros_like(v_channel)
+    c_binary[(s_binary == 1) & (v_binary == 1)] = 1
+    color_binary = np.zeros_like(img[:, :, 0])
+    color_binary[((sxbinary == 1) & (sybinary == 1)) | (c_binary == 1)] = 255
+    return color_binary
 
 
 if __name__ == '__main__':
@@ -14,18 +57,64 @@ if __name__ == '__main__':
     dist = dist_pickle["dist"]
 
     # Read in an image
-    image_file = 'test_images/test6.jpg'
+    image_file = 'test_images/straight_lines1.jpg'
     img = cv2.imread(image_file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # Undistort the image
     undist = cv2.undistort(img, mtx, dist, None, mtx)
 
-    # Display undistorted image
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+    # Colour and gradient transform
+    result = pipeline(undist)
+
+    # Plot the result
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 9))
     f.tight_layout()
+
     ax1.imshow(img)
-    ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(top_down)
-    ax2.set_title('Undistorted Image', fontsize=50)
+    ax1.set_title('Original Image', fontsize=40)
+
+    ax2.imshow(result)
+    ax2.set_title('Pipeline Result', fontsize=40)
+
+    # Warp the image
+    img_size = (img.shape[1], img.shape[0])
+    print(img_size)
+    source = np.float32([[592, 451],
+                         [230, 701],
+                         [1078, 701],
+                         [689, 451]])
+
+    dest = np.float32([[320, 0],
+                       [320, 720],
+                       [960, 720],
+                       [960, 0]])
+    M = cv2.getPerspectiveTransform(source, dest)
+    warped = cv2.warpPerspective(result, M, img_size, flags=cv2.INTER_LINEAR)
+
+    ax3.imshow(warped)
+    ax3.set_title('Warped', fontsize=40)
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
+
+
+    '''# Display undistorted image - uncomment this to see the lines
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 6))
+    f.tight_layout()
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=20)
+    ax2.imshow(undist)
+    ax2.set_title('Undistorted Image', fontsize=20)
+    sx = [source[0, 0], source[1, 0], source[2, 0], source[3, 0]]
+    sy = [source[0, 1], source[1, 1], source[2, 1], source[3, 1]]
+    sline = Line2D(sx, sy, color='r', marker='.')
+    ax2.add_line(sline)
+    ax3.imshow(warped)
+    ax3.set_title('Warped Image', fontsize=20)
+    dx = [dest[0, 0], dest[1, 0], dest[2, 0], dest[3, 0]]
+    dy = [dest[0, 1], dest[1, 1], dest[2, 1], dest[3, 1]]
+    dline = Line2D(dx, dy, color='b', marker='.')
+    ax3.add_line(dline)
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    plt.show()'''
+
--- undistort.py
+++ undistort.py
@@ -1,56 +1,25 @@
-import pickle
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from scipy.spatial import distance
 
-# Read in the saved camera matrix and distortion coefficients
-dist_pickle = pickle.load(open("camera_cal/dist_pickle.p", "rb"))
-mtx = dist_pickle["mtx"]
-dist = dist_pickle["dist"]
-
-# Read in an image
-image_file = 'camera_cal/calibration4.jpg'
-img = cv2.imread(image_file)
-nx = 9  # the number of inside corners in x
-ny = 6  # the number of inside corners in y
-
-def corners_unwarp(img, nx, ny, mtx, dist):
-    dst = cv2.undistort(img, mtx, dist, None, mtx)
-    gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
-    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
-    # If corners found:
-    if ret:
-        image = cv2.drawChessboardCorners(dst, (nx, ny), corners, ret)
-        img_size = (image.shape[1], img.shape[0])
-        adj_corners = np.squeeze(corners)
-        stl = adj_corners[0] # [0] - 436,114 - x,y
-        s_tr = adj_corners[nx-1] # [7] - 1103,224
-        sbr = adj_corners[-1] # [47] - 1075,658
-        sbl = adj_corners[nx*ny-nx] # [40] - 465,768
-        source = np.array([stl,s_tr,sbr,sbl])
-        tl = [100,100]
-        tr = [img_size[0]-100,100]
-        br = [tr[0],img_size[1]-100]
-        bl = [tl[0],br[1]]
-        dest = np.float32([tl, tr, br, bl])
-        M = cv2.getPerspectiveTransform(source, dest)
-        warped = cv2.warpPerspective(image, M, img_size, flags=cv2.INTER_LINEAR)
-    else:
-        warped = np.zeros_like(img)
-        M = 0
-        print("Cannot warp the {} image".format())
-    return warped, M
-
-
-top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
-f.tight_layout()
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(top_down)
-ax2.set_title('Undistorted and Warped Image', fontsize=50)
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-plt.show()
+def warp_image(img):
+    img_size = (img.shape[1], img.shape[0])
+    print(img_size)
+    source = np.float32([[587,451],
+                        [230,701],
+                        [1078,701],
+                        [695,451]]) # best so far
+    '''source = np.float32([[(img_size[0] / 2) - 65, (img_size[1] /2 + 100)],
+                         [((img_size[0] / 6) - 10), img_size[1]],
+                         [(img_size[0] * 5/6) + 60, img_size[1]],
+                         [(img_size[0] / 2 + 65), img_size[1] / 2 + 100]])'''
+    dest = np.float32([[320,0],
+                      [320,720],
+                      [960,720],
+                      [960,0]])
+    '''dest = np.float32([[(img_size[0] / 4), 0],
+                       [(img_size[0] / 4), img_size[1]],
+                       [img_size[0] * 3 /4, img_size[1]],
+                       [img_size[0] * 3 / 4, 0]])'''
+    M = cv2.getPerspectiveTransform(source, dest)
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    return warped
